2917a9c4e01eadb363dff656c174f0a7149c37eb
Reece Donovan <reecedonovan99@gmail.com>
2024-10-12 02:48:59 +0100

    Update BookList.test.tsx to use screen object for querying elements in tests

--- bookmanager-frontend/src/test/components/BookList.test.tsx
+++ bookmanager-frontend/src/test/components/BookList.test.tsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 
 import { configureStore } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import BooksList from '../../components/BooksList';
 import booksReducer from '../../features/bookReducer';
@@ -29,14 +29,14 @@ describe('BooksList', () => {
     });
 
     it('should render a list of books', () => {
-        const { getByText } = render(
+        render(
             <Provider store={store}>
                 <BooksList />
             </Provider>
         );
 
-        expect(getByText('Books')).toBeInTheDocument();
-        expect(getByText('Book One by Author One (Published: 2021-01-01)')).toBeInTheDocument();
-        expect(getByText('Book Two by Author Two (Published: 2022-02-02)')).toBeInTheDocument();
+        expect(screen.getByText('Books')).toBeInTheDocument();
+        expect(screen.getByText('Book One by Author One (Published: 2021-01-01)')).toBeInTheDocument();
+        expect(screen.getByText('Book Two by Author Two (Published: 2022-02-02)')).toBeInTheDocument();
     });
 });
